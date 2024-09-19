f9c444b21deb28668efb6a807034d32bc137a6bb
Vishal Virani <55572605+vvvirani@users.noreply.github.com>
2024-09-19 09:32:19 +0530

    remove unused files

--- scripts/build_files.sh
+++
@@ -1,7 +0,0 @@
-echo 'Deleting old Files...'
-find . -maxdepth 20 -type f \( -name "*.inject.summary" -o -name "*.inject.dart" -o  -name "*.g.dart" -o  -name "*.freezed.dart" \) -delete
-
-echo 'Generating Files...'
-flutter packages pub run build_runner build --delete-conflicting-outputs
-
-echo 'Completed! ✅'
